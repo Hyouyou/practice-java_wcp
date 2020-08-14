3d015672ae93e42de2d64b4f8d05b59e4932af04
Hyouyou <stbn1225@gmail.com>
2020-08-14 10:31:34 +0900

    Lesson1~4

---
+++ src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "my name";
+		System.out.println(name);
+
+		name = "your name";
+		System.out.println(name);
+
+		final int LUCK_NUM = 7;
+		System.out.println(LUCK_NUM);
+	}
+}
---
+++ src/Chapter05.java
@@ -0,0 +1,28 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		/*プリミティブ型*/
+		long long1 = 12345;
+		double dl1 = 123.45;
+		boolean bl1 = true;
+
+		System.out.println(long1);
+		System.out.println(dl1);
+		System.out.println(bl1);
+
+		/*ラッパークラスにボクシング*/
+		Float float1 = 0.123f;
+		Integer integer1 = 5;
+		Character  character1 = 'あ';
+
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(character1);
+
+		/*配列*/
+		int[] array2 = new int[] { 0, 10, 20, 30, 40 };
+		int int2 = array2[3];
+		System.out.println(int2);
+		System.out.println(array2[4]);
+	}
+}
