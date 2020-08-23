90e91b4b905f788cb3440a4cb75b2bcdb3dcf1eb
Hyouyou <stbn1225@gmail.com>
2020-08-23 20:36:22 +0900

    Lesson15-1/my_answer

---
+++ src/chapter/Chapter15_1.java
@@ -0,0 +1,19 @@
+package chapter;
+
+public class Chapter15_1 {
+	public static void main(String[] args) {
+        int a = 976;
+        int b = 427;
+        System.out.println(a + " と " + b + " の最大公約数は、" + gcd(a, b) + "です。");
+    }
+
+    // ユークリッドの互除法で最大公約数を計算するメソッド
+    static int gcd(int a, int b) {
+    	//自分で考えたコード
+    	if (a % b != 0) {
+    		int remain = a % b;
+    		return gcd(b,remain);
+    	}
+    	return b;
+    }
+}
